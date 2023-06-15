f7ee05d6751a602916610950130c794cb2a8c623
piraveenan1234 <136680399+piraveenan1234@users.noreply.github.com>
2023-06-15 12:17:07 +0530

    Add files via upload

---
+++ piraveenan.py
@@ -0,0 +1,7 @@
+Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+===================================================== RESTART: C:/Users/Dell/AppData/Local/Programs/Python/Python311/piraveenan.py =====================================================
+Hello,World!
+What is your name?Piraveenan
+Hello Piraveenan
